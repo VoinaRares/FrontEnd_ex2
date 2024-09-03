5dfc075036c822e111231d8443ca5d6b5d109edf
VoinaRares <voinarares14@gmail.com>
2024-09-03 15:15:51 +0300

    Improved SVG and layout of Page

--- scripts/Home_Page.js
+++ scripts/Home_Page.js
@@ -1,33 +1,9 @@
-/*function resizeSideBar(){
-
-    const sidebar = document.getElementById("sidebar");
-    const width = window.innerWidth;
-    const links = sidebar.getElementsByTagName("a");
-
-    if (width > 1200) {
-        sidebar.style.width = '160px';
-        for(let link of links){
-            link.style.width = '40px';
-        }
-    } else if (width > 768) {
-       sidebar.style.width = '145px';
-       for(let link of links){
-        link.style.width = '25px';
-    }
-    } else if (width > 480) {
-        sidebar.style.width = '130px';
-        for(let link of links){
-            link.style.width = '20px';
-        }
-    } else {
-        sidebar.style.width = '120px';
-        for(let link of links){
-            link.style.width = '15px';
-        }
-    }
+function floorAlert(el){
+    let floorNumber = el.getAttribute("data-floor");
+    alert("You have reached floor " + floorNumber);
 }
 
-resizeSideBar();
 
-
-window.addEventListener('resize', resizeSideBar);*/
+function logOut(){
+    alert("You have been succesfully logged out");
+}
